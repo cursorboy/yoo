34ef62c9b3eaaf545eae4fd4fc2fbaf022fb67f9
Piam <piamparekh17@gmail.com>
2024-11-22 19:32:12 -0800

    trying again

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
   
